578c4cb7f8f6db8289f57fd55c28c85c5bb2da3d
Pandula1234 <52339029+Pandula1234@users.noreply.github.com>
2020-09-17 23:38:12 +0530

    Update movie.scala

--- Scala Assign2- 18001149/movie.scala
+++ Scala Assign2- 18001149/movie.scala
@@ -4,37 +4,26 @@ object movie {
      var list = List(h1,h2,h3)          // Creating a list  
         for( i <- list){
      var net:Double = 0
-	  var no:Int=i-120;
-	 var chan:Int=no/20;
-	 var tic:Double=ticket + 5*chan;
+	 var attendance:Int=120 + (15 - i)/5*20;
 	 
-	 if(chan>0)
-	 { 
-	    net=i*(ticket)+i*5*chan;
-	  }
-      
-	  else
-	  {
-	   net=i*tic;
-	  }
+	    net=i*attendance;
 	  
-	 var netpay:Double=net-perform-i*3;
+	 var netpay:Double=net-perform-attendance*3;
 		
              println( "Net Profit In Theater : " + netpay ); 
-			 println( "The Ticket Price: " + tic ); 
+			 //println( "The Ticket Price: " + tic ); 
         } 
 		
 	    
    }
    
-   var ticket:Double=15.00;
    var perform:Double=500.00;
    
     val h1=scala.io.StdIn.readInt();
 	val h2=scala.io.StdIn.readInt();
 	val h3=scala.io.StdIn.readInt();
-	  println( "Number of Attendance: " + h1 );//First Input attendance
-	  println( "Number of Attendance: " + h2 );//Second Input attendance
-	  println( "Number of Attendance: " + h3 );//Third Input attendance
+	  println( "Ticket Price 1: " + h1 );//First Input attendance
+	  println( "Ticket Price 2: " + h2 );//Second Input attendance
+	  println( "Ticket Price 3: " + h3 );//Third Input attendance
 	         
 }			
